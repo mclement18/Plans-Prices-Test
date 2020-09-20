bc33e020944a9b81c1d1123289cc93f42c05518f
Mathieu Clément <mcfly1893@gmail.com>
2020-09-20 12:24:52 +0200

    Remove Next.js and hardcode info not present in API response

---
+++ data/additional_info.js
@@ -0,0 +1,30 @@
+export const additionalInfo = {
+  free: {
+    Description: 'The basics for private and secure communications',
+    Features: 0,
+    PrioritySupport: false,
+    MostPopular: false,
+    MultiUser: 0
+  },
+  plus: {
+    Description: 'Full-featured mailbox with advanced protection',
+    Features: 'Supports folders, labels, filters, auto-reply, IMAP/SMTP and more',
+    PrioritySupport: false,
+    MostPopular: true,
+    MultiUser: 0
+  },
+  professional: {
+    Description: 'ProtonMail for professionals and businesses',
+    Features: 'Catch all email, multi user management and more',
+    PrioritySupport: true,
+    MostPopular: false,
+    MultiUser: 5000
+  },
+  visionary: {
+    Description: 'ProtonMail for families and small businesses',
+    Features: 'Includes all features',
+    PrioritySupport: true,
+    MostPopular: false,
+    MultiUser: 0
+  }
+};
---
+++ data/free_plan.js
@@ -0,0 +1,13 @@
+export const freePlan = {
+  Amount: 500,
+  Currency: "EUR",
+  ID: "somerandomeid34809841098192808",
+  MaxAddresses: 1,
+  MaxDomains: 0,
+  MaxMembers: 1,
+  MaxSpace: 524288000,
+  MaxVPN: 0,
+  Name: "free",
+  Pricing: 0
+};
+
