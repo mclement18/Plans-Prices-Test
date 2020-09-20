d2633c332a30bff49924d82e85786c15b0be6eef
Mathieu Clément <mcfly1893@gmail.com>
2020-09-21 00:52:58 +0200

    Hardcode currencies and cycles select options

---
+++ data/select_inputs.js
@@ -0,0 +1,29 @@
+export const cycles = [
+  {
+    value: 1,
+    label: 'Monthly'
+  },
+  {
+    value: 12,
+    label: 'Annually'
+  },
+  {
+    value: 24,
+    label: '2 years'
+  }
+];
+
+export const currencies = [
+  {
+    value: 'EUR',
+    label: '\u20AC Euro'
+  },
+  {
+    value: 'CHF',
+    label: 'CHF Swiss Francs'
+  },
+  {
+    value: 'USD',
+    label: '\uFF04 US Dollars'
+  }
+];
