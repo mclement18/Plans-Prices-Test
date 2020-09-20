6815418e720045887cfa1d9e1b7ba8e3d3c4b601
Mathieu Clément <mcfly1893@gmail.com>
2020-09-20 17:00:07 +0200

    Implement data parsing functions

---
+++ utils/data_parsing.js
@@ -0,0 +1,102 @@
+// Storage info parsing
+const convertByteSizeRecursively = (size = 0, n = 0) => {
+  if (size >= 2**10) {
+    return convertByteSizeRecursively(size / 2**10, n + 1);
+  } else {
+    return {size, n};
+  }
+};
+
+const units = Object.freeze({
+  0: 'B',
+  1: 'KB',
+  2: 'MB',
+  4: 'GB',
+  5: 'TB'
+});
+
+export const byteSizeToHumanRedable = (byteSize = 0, sizeUnits = units) => {
+  const {size, n} = convertByteSizeRecursively(byteSize);
+  return `${size} ${sizeUnits[n]}`;
+};
+
+
+// Word pluralization
+export const pluralize = (n = 0, word = '') => {
+  if (Math.abs(n) > 1) {
+    word.endsWith('s') ? word += 'es' : word += 's';
+  }
+  return `${n} ${word}`
+};
+
+
+// User info parsing
+export const getUserNb = (nb = 0, max = 0) => {
+  if (max > 0) {
+    return `${nb} - ${pluralize(max, 'user')}`;
+  } else {
+    return pluralize(nb, 'user');
+  } 
+};
+
+export const perUser = (maxUser = 0) => {
+  return maxUser > 0 ? ' per user' : null;
+};
+
+
+// Domain info parsing
+export const getDomainNb = (nb = 0) => {
+  if (nb === 0) {
+    return 'No domain support';
+  } else {
+    return `Supports ${pluralize(nb, 'domain')}`;
+  }
+};
+
+
+// VPN info parsing
+export const getVPNOption = (nb = 0) => {
+  return nb > 0 ? 'Includes ProtonVPN' : 'ProtonVPN (optional)';
+};
+
+
+// Price info parsing
+const priceRounding = price => {
+  if (!Number.isInteger(price)) {
+    price = `&tilde;${price.toFixed(2)}`;
+  }
+
+  return price;
+};
+
+export const monthlyPrice = (pricing, cycle) => {
+  let price = pricing[cycle];
+  price = price / cycle;
+
+  return priceRounding(price / 100);
+};
+
+export const annualPrice = (pricing, cycle) => {
+  let price = pricing[cycle];
+  
+  if (cycle < 12) {
+    price = price * (12 / cycle);
+  } else if (cycle > 12) {
+    price = price / (cycle / 12);
+  }
+
+  return priceRounding(price / 100);
+};
+
+export const currencySymbol = (currency = 'EUR') => {
+  switch (currency) {
+    case 'EUR':
+      return '&euro;';
+
+    case 'USD':
+      return '&dollar;';
+
+    default:
+      return currency;
+  }
+};
