af087dc043285c5b16a994ee4fbe4c622f38fe63
Mathieu Clément <mcfly1893@gmail.com>
2020-09-21 01:25:00 +0200

    Create PlansAndPrices component in index page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Plan from '../components/Plan';
+import Select from '../components/Select';
+import styles from '../styles/PlansAndPrices.module.scss';
+import { cycles, currencies } from '../data/select_inputs';
+import { extractPlans } from '../utils/data_fetching';
 
-export default function Home() {
+export default function PlansAndPrices() {
+  const [currency, setCurrency] = useState(currencies[0].value);
+  const [cycle, setCycle] = useState(cycles[0].value);
+  const [plans, setPlans] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const plans = await extractPlans(currency);
+      setPlans(plans);
+    })();
+  }, [currency]);
+
+  const onChangeCycle = newCycle => setCycle(newCycle);
+
+  const onChangeCurrency = newCurrency => setCurrency(newCurrency);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Plans and Prices Test</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Plans &amp; Prices</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div className={styles.inputs}>
+          <Select options={cycles} onChangeCallback={onChangeCycle} />
+          <Select options={currencies} onChangeCallback={onChangeCurrency} />
+        </div>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {plans.map(plan => <Plan key={plan.ID} plan={plan} cycle={cycle} />)}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
